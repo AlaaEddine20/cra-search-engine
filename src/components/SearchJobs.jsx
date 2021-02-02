0b1ccbcc925dcc904af03fbf09868af6b1b9d225
Alaa Eddine <alaneddin91@gmail.com>
2021-02-02 15:38:15 +0100

    search done, now component to display jobs

--- src/components/SearchJobs.jsx
+++ src/components/SearchJobs.jsx
@@ -2,16 +2,16 @@ import React, { Component } from "react";
 import { Spinner, Form, Button, Row, Col } from "react-bootstrap";
 export class SearchJobs extends Component {
   state = {
-    keywords: "",
-    keywordsError: false,
+    position: "",
+    positionError: false,
     location: "",
     locationError: false,
-    isSearching: false,
-    jobs: null,
+    isLoading: false,
+    jobs: [],
   };
 
   onKeywordChange = (e) => {
-    this.setState({ keywords: e.target.value });
+    this.setState({ potition: e.target.value });
   };
 
   onLocationChange = (e) => {
@@ -19,7 +19,36 @@ export class SearchJobs extends Component {
   };
 
   clearFields = () => {
-    this.setState({ location: null, keywords: "", jobs: null });
+    this.setState({ location: null, potition: "", jobs: null });
+  };
+
+  searchJobs = (e) => {
+    e.preventDefault();
+    let position = this.state.position;
+    let location = this.state.location;
+    // encoding the values written by the user
+    position = encodeURIComponent(position.trim());
+    location = encodeURIComponent(location.trim());
+    // api url
+    const api = `https://jobs.github.com/positions.json?description=${position}&location=${location}`;
+    const url = "https://jsonp.afeld.me/?url=" + encodeURIComponent(api);
+
+    // user is searching now
+    this.setState({ isLoading: true });
+
+    // fetch
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((res) => {
+        console.log(res);
+        this.setState({ jobs: res, isLoading: false });
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({ isLoading: false });
+      });
   };
 
   render() {
@@ -35,6 +64,7 @@ export class SearchJobs extends Component {
                   value={keywords}
                   onChange={this.onKeywordChange}
                   type="text"
+                  required
                 />
                 <Form.Text className="text-muted">
                   For example: Web developer..
@@ -49,14 +79,19 @@ export class SearchJobs extends Component {
               value={location}
               onChange={this.onLocationChange}
               type="text"
+              required
             />
           </Form.Group>
           <Row>
             <Col>
-              <Button variant="primary" type="submit">
+              <Button onClick={this.searchJobs} variant="primary" type="submit">
                 Search
               </Button>
-              <Button variant="secondary" type="submit">
+              <Button
+                onClick={this.clearFields}
+                variant="secondary"
+                type="submit"
+              >
                 Clear
               </Button>
             </Col>
